8cbfe665bf2b25289467dc5966c272746a83b123
Rohith-Raju <rohithraju488@gmail.com>
2026-01-22 06:31:06 +0100

    update(linear_allocator): use the include and allocator file for defining the address

--- include/linear_allocator.h
+++ include/linear_allocator.h
@@ -1,6 +1,19 @@
-class LinearAllocator{
-	int a;
-	int b;
+#include <cstddef>
+#include <sys/mman.h>
 
-	LinearAllocator(int, int);
+class LinearMemory {
+private:
+  std::byte *memory;
+  unsigned int offset;
+  LinearMemory(std::byte memory, unsigned int offset);
+
+public:
+  void init(std::size_t mem_size,
+            std::size_t allignment = sizeof(std::max_align_t));
+
+  template <typename T> bool assign(T obj);
+
+  void print();
+
+  bool free();
 };
--- src/linear_allocator.cpp
+++ src/linear_allocator.cpp
@@ -1,4 +1,39 @@
 #include "linear_allocator.h"
 
+/*
+class LinearMemory {
+public:
+  char *memory;
+  unsigned int offset;
+  LinearMemory(char *mem) : memory(mem), offset(8) {}
+};
 
-LinearAllocator::LinearAllocator(int a, int b): a(a), b(b) {}
+template <typename T> bool mount_mmu(T, void *raw_mem) {
+  int class_size = sizeof(T);
+}
+
+int main() {
+  char *memory = (char *)mmap(nullptr, MEMORY_SIZE, PROT_READ | PROT_WRITE,
+                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+  if (memory == MAP_FAILED) {
+    std::cout << "Map Failed" << std::endl;
+  } else {
+    std::cout << "Map Succeeded" << std::endl;
+  }
+
+  std::cout << "started at: " << (void *)memory << std::endl;
+
+  LinearMemory *some = new (memory) LinearMemory(memory + sizeof(LinearMemory));
+
+  std::cout << "currently pointed at: " << (void *)some->memory << std::endl;
+
+  int unmapped = munmap(memory, MEMORY_SIZE);
+
+  if (unmapped == -1) {
+    std::cout << "Unmap failed" << std::endl;
+  } else {
+    std::cout << "Unmap completed" << std::endl;
+  }
+}
+*/
