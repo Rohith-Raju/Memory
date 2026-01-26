fcf7b7359dc4b69737c8c0536d7208c448d8a458
Rohith-Raju <rohithraju488@gmail.com>
2026-01-26 22:37:46 +0100

    update(linear): needs fixes but getting the idea of it

--- include/linear_allocator.h
+++ include/linear_allocator.h
@@ -1,15 +1,16 @@
 #include <cstddef>
-#include <sys/mman.h>
 
 class LinearMemory {
 private:
-  std::byte *memory;
-  unsigned int offset;
-  LinearMemory(std::byte memory, unsigned int offset);
+  std::byte *memory_start;
+  std::byte *current;
+  std::byte *soft_end;
+  std::byte *hard_end;
+  LinearMemory(std::byte *memory, unsigned int offset);
 
 public:
-  void init(std::size_t mem_size,
-            std::size_t allignment = sizeof(std::max_align_t));
+  LinearMemory *init(std::size_t mem_size,
+                     std::size_t allignment = sizeof(std::max_align_t));
 
   template <typename T> bool assign(T obj);
 
@@ -17,3 +18,41 @@ public:
 
   bool free();
 };
+
+/*
+class LinearMemory {
+public:
+  char *memory;
+  unsigned int offset;
+  LinearMemory(char *mem) : memory(mem), offset(8) {}
+};
+
+template <typename T> bool mount_mmu(T, void *raw_mem) {
+  int class_size = sizeof(T);
+}
+
+int main() {
+  char *memory = (char *)mmap(nullptr, MEMORY_SIZE, PROT_READ | PROT_WRITE,
+                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+  if (memory == MAP_FAILED) {
+    std::cout << "Map Failed" << std::endl;
+  } else {
+    std::cout << "Map Succeeded" << std::endl;
+  }
+
+  std::cout << "started at: " << (void *)memory << std::endl;
+
+  LinearMemory *some = new (memory) LinearMemory(memory + sizeof(LinearMemory));
+
+  std::cout << "currently pointed at: " << (void *)some->memory << std::endl;
+
+  int unmapped = munmap(memory, MEMORY_SIZE);
+
+  if (unmapped == -1) {
+    std::cout << "Unmap failed" << std::endl;
+  } else {
+    std::cout << "Unmap completed" << std::endl;
+  }
+}
+*/
--- src/linear_allocator.cpp
+++ src/linear_allocator.cpp
@@ -1,39 +1,13 @@
 #include "linear_allocator.h"
+#include <new>
+#include <sys/mman.h>
 
-/*
-class LinearMemory {
-public:
-  char *memory;
-  unsigned int offset;
-  LinearMemory(char *mem) : memory(mem), offset(8) {}
-};
+LinearMemory::LinearMemory(std::byte *memory, unsigned int offset)
+    : memory(memory_start), offset(offset) {}
 
-template <typename T> bool mount_mmu(T, void *raw_mem) {
-  int class_size = sizeof(T);
+auto LinearMemory::init(std::size_t mem_size, std::size_t allignment) {
+  std::byte *memory =
+      (std::byte *)mmap(nullptr, mem_size, PROT_READ | PROT_WRITE,
+                        MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+  return new (memory) LinearMemory(sizeof(LinearMemory) + memory, 8);
 }
-
-int main() {
-  char *memory = (char *)mmap(nullptr, MEMORY_SIZE, PROT_READ | PROT_WRITE,
-                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-  if (memory == MAP_FAILED) {
-    std::cout << "Map Failed" << std::endl;
-  } else {
-    std::cout << "Map Succeeded" << std::endl;
-  }
-
-  std::cout << "started at: " << (void *)memory << std::endl;
-
-  LinearMemory *some = new (memory) LinearMemory(memory + sizeof(LinearMemory));
-
-  std::cout << "currently pointed at: " << (void *)some->memory << std::endl;
-
-  int unmapped = munmap(memory, MEMORY_SIZE);
-
-  if (unmapped == -1) {
-    std::cout << "Unmap failed" << std::endl;
-  } else {
-    std::cout << "Unmap completed" << std::endl;
-  }
-}
-*/
