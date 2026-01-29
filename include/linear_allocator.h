0daf3aa2e942b9fa1de22d47ab2554f456c22215
Rohith-Raju <rohithraju488@gmail.com>
2026-01-29 06:30:36 +0100

    update(linear): fixed constructor and figured out alignment. still neeeds padding

--- include/linear_allocator.h
+++ include/linear_allocator.h
@@ -6,53 +6,16 @@ private:
   std::byte *current;
   std::byte *soft_end;
   std::byte *hard_end;
-  LinearMemory(std::byte *memory, unsigned int offset);
+  LinearMemory(std::byte *memory_start, std::byte *current, std::byte *soft_end,
+               std::byte *hard_end);
 
 public:
-  LinearMemory *init(std::size_t mem_size,
-                     std::size_t allignment = sizeof(std::max_align_t));
+  void init(std::size_t mem_size,
+            std::size_t allignment = sizeof(std::max_align_t));
 
-  template <typename T> bool assign(T obj);
+  template <typename T> void *assign(T obj);
 
-  void print();
+  void print_stats();
 
   bool free();
 };
-
-/*
-class LinearMemory {
-public:
-  char *memory;
-  unsigned int offset;
-  LinearMemory(char *mem) : memory(mem), offset(8) {}
-};
-
-template <typename T> bool mount_mmu(T, void *raw_mem) {
-  int class_size = sizeof(T);
-}
-
-int main() {
-  char *memory = (char *)mmap(nullptr, MEMORY_SIZE, PROT_READ | PROT_WRITE,
-                              MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-
-  if (memory == MAP_FAILED) {
-    std::cout << "Map Failed" << std::endl;
-  } else {
-    std::cout << "Map Succeeded" << std::endl;
-  }
-
-  std::cout << "started at: " << (void *)memory << std::endl;
-
-  LinearMemory *some = new (memory) LinearMemory(memory + sizeof(LinearMemory));
-
-  std::cout << "currently pointed at: " << (void *)some->memory << std::endl;
-
-  int unmapped = munmap(memory, MEMORY_SIZE);
-
-  if (unmapped == -1) {
-    std::cout << "Unmap failed" << std::endl;
-  } else {
-    std::cout << "Unmap completed" << std::endl;
-  }
-}
-*/
--- src/linear_allocator.cpp
+++ src/linear_allocator.cpp
@@ -1,13 +1,31 @@
 #include "linear_allocator.h"
+#include <iostream>
 #include <new>
 #include <sys/mman.h>
 
-LinearMemory::LinearMemory(std::byte *memory, unsigned int offset)
-    : memory(memory_start), offset(offset) {}
+LinearMemory::LinearMemory(std::byte *memory_start, std::byte *current,
+                           std::byte *soft_end, std::byte *hard_end)
+    : memory_start(memory_start), current(current), soft_end(soft_end),
+      hard_end(hard_end) {}
 
-auto LinearMemory::init(std::size_t mem_size, std::size_t allignment) {
+void LinearMemory::init(std::size_t mem_size, std::size_t allignment) {
   std::byte *memory =
       (std::byte *)mmap(nullptr, mem_size, PROT_READ | PROT_WRITE,
                         MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-  return new (memory) LinearMemory(sizeof(LinearMemory) + memory, 8);
+
+  std::cout << "Memory assined at" << (void *)memory;
+
+  std::byte *start = sizeof(LinearMemory) + memory;
+  new (memory) LinearMemory(start, start, start + mem_size, start + mem_size);
+}
+
+void LinearMemory::print_stats() {
+  std::cout << "starting address: " << (void *)memory_start << std::endl;
+  std::cout << "current address : " << (void *)current << std::endl;
+  std::cout << "soft_end address : " << (void *)soft_end << std::endl;
+  std::cout << "hard_end address : " << (void *)hard_end << std::endl;
+}
+
+template <typename T> void *assign(T obj) {
+  std::size_t class_size = sizeof(T);
 }
